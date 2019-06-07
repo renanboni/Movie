88e4a4a87df3604a14c4d19b6f11c25601a3f035
renanboni <renan-boni@hotmail.com>
2019-06-07 20:09:19 -0300

    Changing network call and dispose from build to lifecyle methods

--- lib/src/ui/movie_list.dart
+++ lib/src/ui/movie_list.dart
@@ -2,11 +2,28 @@ import 'package:flutter/material.dart';
 import '../models/item_model.dart';
 import '../blocs/movies_bloc.dart';
 
-class MovieList extends StatelessWidget {
+class MovieList extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MovieListState();
+  }
+}
+
+class MovieListState extends State<MovieList> {
+  @override
+  void initState() {
+    super.initState();
+    bloc.fetchAllMovies();
+  }
+
+  @override
+  void dispose() {
+    bloc.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    bloc.fetchAllMovies();
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Popular Movies'),
@@ -29,11 +46,14 @@ class MovieList extends StatelessWidget {
     return GridView.builder(
         itemCount: snapshot.data.results.length,
         gridDelegate:
-            new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
         itemBuilder: (BuildContext context, int index) {
-          return Image.network(
-            'https://image.tmdb.org/t/p/w185${snapshot.data.results[index].poster_path}',
-            fit: BoxFit.cover,
+          return GridTile(
+            child: Image.network(
+              'https://image.tmdb.org/t/p/w185${snapshot.data
+                  .results[index].poster_path}',
+              fit: BoxFit.cover,
+            ),
           );
         });
   }
